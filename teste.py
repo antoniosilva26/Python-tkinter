bd0d89597ec1a6f13bc5fe9208b237fa3c6a9630
Toni Silva <baianodeposito9@gmail.com>
2026-05-16 15:13:42 -0300

    Atualizando e colocando novas funções
    Sitema básico de cadastro com tkinter

--- teste.py
+++ teste.py
@@ -1,5 +1,7 @@
 from tkinter import * 
 import tkinter as tk
+from tkinter import messagebox
+import time
 
 janela = tk.Tk()
 
@@ -17,10 +19,13 @@ class Configs():
         entry_nome = tk.Entry(label2, width=30, show='*').pack(pady=10)
         label4 = tk.Label(label2, text="Senha", bg="lightblue", font='Arial 12').pack(pady=5)
         entry_senha = tk.Entry(label2, width=30, show='*').pack(pady=10)
-        BotaoCadastro = Button(label2, text="Cadastro")
+        BotaoCadastro = Button(label2, text="Cadastro", command=self.Cadastro)
         BotaoCancelar = Button(label2, text="Cancelar")
         BotaoCadastro.place(y=180, x=102)
         BotaoCancelar.place(y=210, x=102)
+    def Cadastro(self):
+        msg = messagebox.showinfo("Cadastro", "Cadastro concluido com sucesso!")
+        janela.destroy()
 class Aplication():
     def __init__(self):
         self.janela = janela
